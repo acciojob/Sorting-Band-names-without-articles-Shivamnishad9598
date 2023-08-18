9a30a4cda70e1501a0147ae30a72b02edd9e026b
Shivam Kumar Nishad <111196780+Shivamnishad9598@users.noreply.github.com>
2023-08-18 14:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+const bands = ["The Beatles", "The Rolling Stones", "AC/DC"];
 
+const removeArticles = (bandName) => {
+  const articles = ["a", "an", "the"];
+  for (const article of articles) {
+    bandName = bandName.replace(article, "");
+  }
+  return bandName;
+};
+
+const sortedBands = bands.map(removeArticles).sort();
+
+const ul = document.getElementById("band");
+for (const band of sortedBands) {
+  const li = document.createElement("li");
+  li.textContent = band;
+  ul.appendChild(li);
+}
